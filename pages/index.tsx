3b86400022b8a60eec9751f46d5a579155451b1d
electron <nyhmsaint@gmail.com>
2022-04-07 01:30:18 -0400

    Hmm

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,8 @@
 import type { NextPage } from 'next';
+import { Button, ButtonGroup } from '@chakra-ui/react';
 
 const Home: NextPage = () => {
-	return (
-		<div>
-			<Button colorScheme='blue'>Hello</Button>
-		</div>
-	);
+	return <div>{/* <Button colorScheme='blue'>Hello</Button> */}</div>;
 };
 
 export default Home;

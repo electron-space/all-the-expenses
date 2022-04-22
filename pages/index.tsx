ea7036d232c57563dca859283cbf7c729c14971a
electron <nyhmsaint@gmail.com>
2022-04-21 23:51:38 -0400

    cHECK

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import { Button, ButtonGroup } from '@chakra-ui/react';
-import { VStack } from '@chakra-ui/react';
+
 const Home: NextPage = () => {
 	return <div>{/* <Button colorScheme='blue'>Hello</Button> */}</div>;
 };

56b014f6a2c176b32b5b4d84410b32beaa1c59cd
electron-space <6634301+electron-space@users.noreply.github.com>
2022-05-12 22:16:45 -0400

    Making Changes back for now.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<ChakraProvider>
+			<Component {...pageProps} />
+		</ChakraProvider>
+	);
 }
 
 export default MyApp;
